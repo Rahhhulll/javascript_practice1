5df381930780a080e3e16e21001f0886bba142e4
Rahhhulll <rkrahulrk651@gmail.com>
2024-07-05 14:30:10 +0530

    02_basics javascript

---
+++ 02_basics/array.js
@@ -0,0 +1,33 @@
+// const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr[4])
+
+// const myHeros = ["spiderman", "hulk", "ironman", "shaktiman" ]
+
+// // const myArr2 = new Array(1,2,3,4)
+// // console.log(myArr[2])
+
+
+// //Array Method
+
+// // myArr.push(6)
+// // myArr.push(7)
+
+// // myArr.pop(7)
+
+// // myArr.unshift(7)
+// // myArr.shift(6)
+// // console.log(myArr)
+
+// console.log(myArr.includes(9))
+
+
+//=======================02 Array==========================
+
+const marvelHeros = ["thor", "IronMan" , "spidernan"]
+const dcHeroes = ["superman", "flash", "batman"]
+
+// marvelHeros.push(dcHeroes)
+
+// console.log(marvelHeros)
+marvelHeros.concat(dcHeroes)
+console.log(marvelHeros)
---
+++ 02_basics/objects.js
@@ -0,0 +1,34 @@
+//singleton
+
+//object literals 
+
+// const jsaUser =  {
+//     name : "Rahul",
+//     age : 18,
+//     location : "Pune",
+//     email:"rahul@gmail.com",
+//     isLoggedIn : false,
+//     lastLoginDays : ["Monday", "Saturday"]
+
+// }
+
+// console.log(jsaUser.email)
+// console.log(jsaUser["email"])
+
+const course = {
+    coursename : "js in Hindi",
+    price : "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course
+
+console.log(courseInstructor)
+
+const navbar = () => {
+
+}
+
+navbar()
